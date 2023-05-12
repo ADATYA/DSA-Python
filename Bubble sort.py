6baeda1ab845665b09a13e1beba028af3f815ab4
bikrom <bikromroy0711@gmail.com>
2023-05-12 23:51:58 +0600

    new

--- Bubble sort.py
+++ Bubble sort.py
@@ -50,3 +50,75 @@ ele = [12,45,7,2,8,3,5,17,47,1]
 result = bubblesort(ele)
 
 print(result)
+
+
+#3
+def Bubblesort(element):
+    o = len(element)
+
+    for i in range(o-1):
+        flag = 0
+
+        for j in range(o-1):
+            if element[j] > element[j+1]:
+                temp = element[j]
+                element[j] = element[j+1]
+                element[j+1] = temp 
+                flag = 1 
+
+        if flag == 0 :
+            break
+
+    return element
+
+ele = [22,45,78,98,1,24,56,100,2,0]
+
+result = Bubblesort(ele)
+
+print(result)
+
+def bs(e):
+    h  = len(e)
+
+    for i in range(h-1):
+        flag = 0
+
+        for j in range(h-1):
+
+            if e[j] > e[j+1]:
+                t = e[j]
+                e[j]= e[j+1]
+                e[j+1] =t
+                flag = 1
+
+        if flag ==0:
+         break
+    return e 
+
+e = [22,45,67,90,1,3,57,2,69]
+r = bs(e)
+print(r)
+
+
+def bs (element):
+    x = len(element)
+
+    for i in range(x-1):
+        flag = 0
+
+        for j in range(x-1):
+            if element[j] >element[j+1]:
+                tem =element[j]
+                element[j] = element[j+1]
+                element[j+1] = tem
+                flag = 1
+
+        if flag ==0:
+            break
+
+    return element
+
+EL=[23,45,76,78,1,8,7,4,37]
+
+result = bs(EL)
+print(result)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,63 +1,17 @@
-                 tempurary = paramiter[j]
-# #                 paramiter[j] = paramiter [j+1]
-# #                 paramiter[j+1] = tempurary
-# #                 empty  =1 
+def BinarySearch(list,item):
+#     first =0
+#     last = len(list)-1
+#     found=False
+#     while(first <=last and not found):
+#         mid = (first +last)//2 #//floor divition.
+#         if list[mid] ==item :
+#             found = True
+#         else:
+#             if item <list[mid]:
+#                 last =mid-1
+#             else:
+#                 first =mid-1
+#     return found
 
-# #         if empty ==0:
-# #                 break
-
-# #     return empty
-
-# # ele = [22,6,9,3,33]
-
-# # Show = (BubbleSort(ele))
-
-# # print(Show)
-
-# # def bubbleSort( theSeq ):
-# #     n = len( theSeq )
-
-# #     for i in range( n - 1 ) :
-# #         flag = 0
-
-# #         for j in range(n - 1) :
-            
-# #             if theSeq[j] > theSeq[j + 1] : 
-# #                 tmp = theSeq[j]
-# #                 theSeq[j] = theSeq[j + 1]
-# #                 theSeq[j + 1] = tmp
-# #                 flag = 1
-
-# #         if flag == 0:
-# #             break
-
-# #     return theSeq
-
-# # el = [21,6,9,33,3] 
-
-# # result = bubbleSort(el)
-
-# # print (result)
-
-
-# def bubbleSort(element):
-#     n = len(element)
-
-#     #outer for loop :
-#     for i in range(n-1):
-#         flag = 0
-
-#         for j in range(n-1):
-#             if element[j] > element[j+1]:
-#                 temp = element[j]
-#                 element[j] = element[j+1]
-#                 element[j+1] = temp
-#                 flag = 1 
-#         if flag == 0:
-#              break
-#     return element
-# el = [22,6,9,33,3]
-
-# result =bubbleSort(el) 
-
-# print(result)
+# print(BinarySearch([1,2,3,4,5,6],8))
+# print(BinarySearch([1,2,3,4,5],5))
